d571bd2f8677ccc969f145ffd456c9408a9bfc63
Burnout <jansilvavieira16@gmail.com>
2026-03-17 11:06:40 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController1 : MonoBehaviour
+{
+    public float speed = 20f;
+    public float xRange = 15f;
+    public GameObject projectilePrefab;
+    private float horizontalInput;
+
+    public InputActionAsset InputActions;
+    private InputAction moveAction;
+    private InputAction fireAction;
+    private InputAction pausaActionPlayer;
+    private InputAction pausaActionUI;
+    public GameObject painel;
+
+    // Update is called once per frame  
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable(); 
+    }
+    
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        fireAction = InputSystem.actions.FindAction("Jump");
+        pausaActionPlayer = InputSystem.actions.FindAction("Pausa");
+        pausaActionUI = InputSystem.actions.FindAction("Despausa");
+    }
+    void Update()
+    {
+        float horizontalInput = moveAction.ReadValue<Vector2>().x;
+        // movimenta o player para esquerda e direita a partir da entrada do usu�rio
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+        // mant�m o player dentro dos limites do jogo (eixo x)
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.y);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.y);
+        }
+        if(fireAction.WasPressedThisFrame())
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+        }
+        PauseGame();
+       
+}
+void PauseGame()
+    {
+         if (pausaActionPlayer.WasPressedThisFrame())
+        {
+            painel.SetActive(true);
+            InputActions.FindActionMap("Player").Disable(); 
+            InputActions.FindActionMap("UI").Enable(); 
+        }
+        if (pausaActionUI.WasPressedThisFrame())
+        {
+            painel.SetActive(false);
+            InputActions.FindActionMap("Player").Enable(); 
+            InputActions.FindActionMap("UI").Disable(); 
+        }
+    }
+    }
